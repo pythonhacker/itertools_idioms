06f87701349ed4ce066aa4fcb31df1f2ce6e85c8
Anand B Pillai <abpillai@gmail.com>
2013-09-03 11:41:00 +0530

    Implicit support for comparison operators

--- idioms.py
+++ idioms.py
@@ -8,6 +8,13 @@
 import random
 import itertools
 import string
+import operator
+
+__operators__ = {'>=': operator.ge,
+                 '>': operator.gt,
+                 '<': operator.lt,
+                 '==': operator.eq,
+                 '<=': operator.le}
 
 # With random
 def random_stream(iterable, sentinel=None):
@@ -39,7 +46,7 @@ def random_digits(sentinel=None):
 
 # END - Specific use cases of random_stream
 
-def select(subjects, constraints, cmap={}):
+def select(subjects, constraints, cmap={}, use_operator=False):
     """ Return iterable from subjects matching the given constraints.
     Both are dictionaries sharing the same keys. Constraints
     should expressed as a dictionary with values expressed
@@ -50,9 +57,15 @@ def select(subjects, constraints, cmap={}):
     map constraints to actual functions. This could be useful
     for example when constraints are no expressed as actual
     functions, but function names or strings.
+
+    For comparison operators >,<,>=,<= and ==, implicit support
+    is provided. You can pass the argument use_operator as True
+    to enable this. The cmap argument is ignored in such case.
     """
 
     s, c, m = subjects, constraints, cmap
+    if use_operator:
+        m = __operators__
     if m:
         selectors = {key: m[c[key][0]](s[key], c[key][1]) for key in subjects}
     else:
